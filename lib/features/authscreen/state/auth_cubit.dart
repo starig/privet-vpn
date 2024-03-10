a6b8de6f9912009f2d9f1ddb286e0342bcd83fb9
starig <starkovivan3@gmail.com>
2024-03-11 00:33:28 +0300

    firebase mobile apps

---
+++ lib/features/authscreen/state/auth_cubit.dart
@@ -0,0 +1,32 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthState());
+
+  signInWithGoogle() async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      inspect(e);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:privet_vpn/app.dart';
 import 'package:privet_vpn/config/theme.dart';
+import 'package:privet_vpn/features/authscreen/state/auth_cubit.dart';
 import 'package:privet_vpn/features/home/home_view.dart';
 import 'package:privet_vpn/features/home/state/home_cubit.dart';
 import 'package:privet_vpn/features/login/login_view.dart';
@@ -10,12 +12,22 @@ import 'package:privet_vpn/features/new_password/new_password_view.dart';
 import 'package:privet_vpn/features/profile/profile_view.dart';
 import 'package:privet_vpn/features/register/register_view.dart';
 import 'package:privet_vpn/features/reset_password/reset_password_view.dart';
+import 'package:privet_vpn/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
   runApp(
     MultiBlocProvider(
         providers: [
           BlocProvider(create: ((context) => HomeCubit())),
+          BlocProvider(create: ((context) => AuthCubit())),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
